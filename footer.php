a011cba7281489f72d79e93c8df6058c3f65f993
rinoZakharia <mnfx14@gmail.com.com>
2023-05-17 22:28:05 +0700

    update tab navigation for "barang"

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <!-- /.content-wrapper -->
-    <footer class="main-footer">
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-        <div class="float-right d-none d-sm-inline-block">
-            <b>Version</b> 3.2.0
-        </div>
-    </footer>
+<footer class="main-footer">
+  <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 3.2.0
+  </div>
+</footer>
 
-    <!-- DataTables  & Plugins -->
+<!-- DataTables  & Plugins -->
 <script src="plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
 <script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
@@ -21,9 +21,11 @@
 <script src="plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
 <script>
-  $(function () {
+  $(function() {
     $("#example1").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "responsive": true,
+      "lengthChange": false,
+      "autoWidth": false,
       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     $('#example2').DataTable({
@@ -35,5 +37,21 @@
       "autoWidth": false,
       "responsive": true,
     });
+    // Clear tambah barang
+    $('#custom-tabs-one-profile-tab').on("click", function() {
+      $('#idBarang').val("");
+      $('#namaBarang').val("");
+      $('#proses').val("Tambah");
+    });
   });
+
+  // Edit Barang
+  function editBarang(id, nama) {
+    console.log(id);
+    console.log(nama);
+    $('[href="#custom-tabs-one-profile"]').tab('show');
+    $('#idBarang').val(id);
+    $('#namaBarang').val(nama);
+    $('#proses').val("Update");
+  }
 </script>
